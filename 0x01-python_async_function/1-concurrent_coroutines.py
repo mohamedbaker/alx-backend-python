3b1617864b010d8f61b2eb693ca2018186c1b51a
mohamedbaker <dam.vampire22@gmail.com>
2024-06-11 05:00:22 +0300

    multiable coroutin

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''
+Write an asynchronous coroutine that takes in an integer argument
+(max_delay, with a default value of 10) named wait_random
+'''
+
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''
+     returns the list of all delays in ascending order
+    '''
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
