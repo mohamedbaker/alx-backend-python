25c66184bbc16b5a89d28595dfb38b4f51734fea
mohamedbaker <dam.vampire22@gmail.com>
2024-06-11 05:08:52 +0300

    return asycn io with regular expression

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,15 @@
+"""
+ 3. Tasks : return asyncio.task with reg expression.
+"""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Takes an integer max_delay, creates a task with wait_random(max_delay) and
+    returns it
+    """
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
