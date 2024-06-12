5afbfcfe6b290eac5ffe6b0e08337962b128358d
mohamedbaker <dam.vampire22@gmail.com>
2024-06-12 05:07:03 +0300

    return list of rand nums

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+'''
+Import async_generator
+write a coroutine called async_comprehension that takes no arguments.
+collect 10 rand numbers using an async comprehensing over async_generator,
+then return the 10 random numbers
+'''
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    '''
+    collect 10 random numbers using an async comprehensing.
+    '''
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    return result
